89a9f10b079e5f68d80ebf96e229f46d40657dd8
pandarinadh <39387722+pandarinadh@users.noreply.github.com>
2018-05-23 12:20:47 -0400

    setup react components

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from './components/App';
+import HomePage from './components/home/homePage';
+import AboutPage from './components/about/aboutPage';
+import CoursesPage from './components/courses/coursesPage';
+
+export default (
+    <Route path="/" component={App}>
+        <IndexRoute component={HomePage} />
+        <Route path="courses" component={CoursesPage} />
+        <Route path="about" component={AboutPage} />
+    </Route>
+);
